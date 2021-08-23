0f3ec8882f617499d9f9bd6054c2e7fb5ebcec78
WH <WH@YOGO>
2021-08-23 11:06:00 +0800

    修改文件保存格式

--- bin2txt.py
+++ bin2txt.py
@@ -3,27 +3,28 @@
 import sys  # for open()
 import binascii  # for binascii()
 
-
 def bin2txt(r_path, w_path):
     with open(r_path, 'rb') as r_f:
         with open(w_path, 'w') as w_f:
-            w_f.write("argv[] = {\r")
+            w_f.write("#ifndef __DATA_H__\r#define __DATA_H__\r\runsigned char argv[] = {\r")
             w_f.write("\t")
             counter = 0
             while 1:
                 msg = r_f.read(1)
                 if not msg:
-                    break
-                data_str = "0x" +  binascii.b2a_hex(msg).decode('utf-8') + ", "
-                w_f.write(data_str)
-                counter += 1
-                # 换行
-                if (counter == 8):
-                    counter = 0;
-                    w_f.write("\r")
-                    w_f.write("\t")
+                    w_f.write("\r};\r\r#endif\r")
+                    r_f.close()
+                    w_f.close()
+                    return
 
-            w_f.write("};\r")
-        r_f.close()
-        w_f.close()
+                counter += 1
+                if (counter == 1):
+                    data_str = "0x" +  binascii.b2a_hex(msg).decode('utf-8')
+                else:
+                    data_str = ", 0x" + binascii.b2a_hex(msg).decode('utf-8')
+                w_f.write(data_str)
+                # 换行
+                if (counter >= 8):
+                    counter = 0;
+                    w_f.write(",\r\t")
 
